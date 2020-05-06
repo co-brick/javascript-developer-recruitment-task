2613b2c6b262bfdcb9afbae568cdd91fc4ecce02
Michal Bujalski <bujalski.michal@gmail.com>
2020-05-06 19:11:11 +0200

    Prevent loading value unecessary changes

--- src/app/core/services/loader/loader.service.ts
+++ src/app/core/services/loader/loader.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoaderService {
-  private loadingSubject = new Subject<boolean>();
+  private loadingSubject = new BehaviorSubject<boolean>(false);
   constructor() {}
   setIsLoading(newIsLoading: boolean) {
     this.loadingSubject.next(newIsLoading);
   }
   loading$(): Observable<boolean> {
-    return this.loadingSubject;
+    return this.loadingSubject.pipe(distinctUntilChanged(x=>x));
   }
 }
